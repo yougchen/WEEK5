8b37867f503d98795dbae29db60271a9434c6116
yougchen <milk19961014@gmail.com>
2017-04-03 17:54:32 +0800

    Create display.php

---
+++ display.php
@@ -0,0 +1,23 @@
+<html>
+	<body text="yellow" leftmargin="100" topmargin="100" bgcolor="#ffcccc" background="">
+<?php
+echo "<font size='500'>";
+include("time.php");
+echo "</font>";
+include("picture.php");
+
+echo "<br/>";
+
+echo "<font color='red' size='10000'>";
+echo "世界末日倒數計時:";
+include("ding.php");
+echo "</font>";
+
+
+
+
+
+?>
+	</body>
+
+</html>
