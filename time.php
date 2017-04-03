0172f222ed006caafc7822bfa989e7c362894f84
yougchen <milk19961014@gmail.com>
2017-04-03 17:56:06 +0800

    Create time.php

---
+++ time.php
@@ -0,0 +1,383 @@
+<html>
+	
+<?php
+date_default_timezone_set("Asia/Taipei");
+echo date("Y年");
+if(date("F")=="January"){
+	echo "一月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+
+
+}
+
+if(date("F")=="February"){
+	echo "二月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="March"){
+	echo "三月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="April"){
+	echo "四月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="May"){
+	echo "五月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="June"){
+	echo "六月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="July"){
+	echo "七月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="August"){
+	echo "八月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="September"){
+	echo "九月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="October"){
+	echo "十月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="November"){
+	echo "十一月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+if(date("F")=="December"){
+	echo "十二月";
+	if(date("l")=="Sunday"){
+		echo date("j日星期日 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Monday"){
+		echo date("j日星期一 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Tuesday"){
+		echo date("j日星期二 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Wednesday"){
+		echo date("j日星期三 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Thursday"){
+		echo date("j日星期四 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Friday"){
+		echo date("j日星期五 H:i:s")."<br/>";
+	}
+
+	if(date("l")=="Saturday"){
+		echo date("j日星期六 H:i:s")."<br/>";
+	}
+}
+
+?>
+
+	
+</html>
