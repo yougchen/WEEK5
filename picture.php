35ca52220227afecfeb732d594bb2792fa92016f
yougchen <milk19961014@gmail.com>
2017-04-03 17:55:05 +0800

    Create picture.php

---
+++ picture.php
@@ -0,0 +1,53 @@
+<?php
+
+if(date("F")=="January"){
+	echo "<img src='https://i.ytimg.com/vi/yLyGyRLoIPQ/hqdefault.jpg' width='50%' border='0'>";
+}
+if(date("F")=="February"){
+	echo "<img src='https://i.ytimg.com/vi/ZYV8c7Eu1VA/maxresdefault.jpg' width='50%' border='0'>";
+
+}
+if(date("F")=="March"){
+	echo "<img src='https://i.ytimg.com/vi/tqH5UJjz93A/maxresdefault.jpg' width='50%' border='0'>";
+}
+if(date("F")=="April"){
+	echo "<img src='http://pansci.asia/wp-content/uploads/2015/11/6bb61e3b7bce0931da574d19d1d82c881.jpg' width='50%' border='0'>";
+
+}
+if(date("F")=="May"){
+	echo "<img src='https://d18gmz9e98r8v5.cloudfront.net/ptr/20141125094055_1464699934_10593_9.jpg' width='50%' border='0'>";
+
+}
+if(date("F")=="June"){
+	echo "<img src='https://i.ytimg.com/vi/TVI_Z_HfdxM/maxresdefault.jpg' width='50%' border='0'>";
+
+}
+if(date("F")=="July"){
+	echo "<img src='https://churaumi.okinawa/userfiles/images/guide/image-01.jpg' width='50%' border='0'>";
+
+}
+if(date("F")=="August"){
+	echo "<img src='http://opinion.cw.com.tw/sites/default/files/blog_articles/main_image/images/1021_%E8%AE%80%E8%80%85_%E4%B8%BB%E5%9C%96.jpg' width='50%' border='0'>";
+
+}
+if(date("F")=="September"){
+	echo "<img src='http://cw1.tw/CW/images/article/C1460100244034.jpg' width='50%' border='0'>";
+
+}
+if(date("F")=="October"){
+	echo "<img src='http://cw1.tw/CW/images/article/C1460100074525.jpg' width='50%' border='0'>";
+
+}
+if(date("F")=="November"){
+	echo "<img src='http://spaceresource.dadunet.com/artweb/2011/09/07/14/50617.jpg' width='50%' border='0'>";
+
+}
+if(date("F")=="December"){
+	echo "<img src='https://i.ytimg.com/vi/krCHsgF2ETY/hqdefault.jpg' width='50%' border='0'>";
+
+}
+
+
+
+
+?>
